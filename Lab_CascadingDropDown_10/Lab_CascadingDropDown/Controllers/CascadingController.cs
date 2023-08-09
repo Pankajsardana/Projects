d5650d95a2e190d6ba06c234aa85e103625e2467
Pankajsardana <54014778+Pankajsardana@users.noreply.github.com>
2023-08-08 22:08:44 -0700

    Trigger rebuild

---
+++ Lab_CascadingDropDown_10/Lab_CascadingDropDown/Controllers/CascadingController.cs
@@ -0,0 +1,32 @@
+﻿using DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_CascadingDropDown.Controllers
+{
+    public class CascadingController : Controller
+    {
+        AppDBContext _appDBContext;
+        public CascadingController(AppDBContext appDBContext)
+        {
+                _appDBContext = appDBContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Country() 
+        {
+            var co=_appDBContext.Countries.ToList();
+            return new JsonResult(co);
+
+        }
+
+        public JsonResult City(int id) 
+        { 
+            var ci = _appDBContext.Cities.Where(e => e.Country.CountryId == id).ToList(); 
+            return new JsonResult(ci);
+        }
+    }
+}
