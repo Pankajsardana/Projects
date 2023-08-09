466e97d358c2191bea21d20ba8c39d2d98c41698
Pankajsardana <54014778+Pankajsardana@users.noreply.github.com>
2023-08-08 22:14:37 -0700

    Trigger rebuild

---
+++ Lab-PagingSortingSearching_12/Lab-PagingSortingSearching/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using DAL;
+using DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab_PagingSortingSearching.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly AppDBContext _appDBContext;
+        public ProductController(AppDBContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts() 
+        {
+            if(_appDBContext.Products == null) 
+            {
+                return Problem("Entities set are empty:AppDBContext");
+            }
+            return await _appDBContext.Products.ToListAsync();
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<Product>> GetProducts(string SearchText = "") 
+        {
+            var query=_appDBContext.Products.AsQueryable();
+
+            if (!string.IsNullOrEmpty(SearchText)) 
+            {
+                query=_appDBContext.Products.Where(p=>p.Name.Contains(SearchText)).AsQueryable();
+
+            }
+
+            return  Ok(query.ToListAsync());
+        }
+
+        
+    }
+}
---
+++ Lab-PagingSortingSearching_12/Lab-PagingSortingSearching/Models/Pager.cs
@@ -0,0 +1,58 @@
+﻿namespace Lab_PagingSortingSearching.Models
+{
+    public class Pager
+    {
+        public Pager()
+        {
+
+        }
+
+        public string SearchText { get; set; } = "";
+        public string Controller { get; set; } = "";
+        public string Action { get; set; } = "Index";
+
+        public int TotalItems { get; private set; }
+        public int CurrentPage { get; private set;}
+        public int PageSize { get; private set;}
+        public int TotalPages { get; private set;}
+
+        public int StartPage { get; private set; }
+        public int EndPage { get; private set; }
+
+        public int StartRecord { get; private set;}
+        public int EndRecord { get; private set; }
+
+        public Pager(int totalItems, int page,int pageSize=10) 
+        {
+            totalItems = (int)Math.Ceiling((Decimal)totalItems/(Decimal)pageSize);
+            int currentpage = page;
+            int startPage = currentpage - 5;
+            int endPage = currentpage + 5;
+            if(startPage<=0)
+            {
+                endPage = endPage - (startPage - 1);
+                startPage = 1;
+            }
+            if(endPage>startPage)
+            {
+                endPage = totalItems;
+                if (endPage > 10) 
+                { 
+                    startPage = endPage - 9;
+                }
+            }
+            TotalItems = totalItems; 
+            CurrentPage = currentpage; 
+            PageSize = pageSize; 
+            TotalPages = totalItems; 
+            StartPage = startPage; 
+            EndPage = endPage; 
+            StartRecord = (CurrentPage - 1) * PageSize + 1; 
+            EndRecord = StartRecord - 1 + PageSize;
+
+        }
+
+
+
+    }
+}
